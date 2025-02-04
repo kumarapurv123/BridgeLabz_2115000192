d344674f33cd1358e819c38d3af14765407de1f2
Apurv Kumar <91779441+kumarapurv123@users.noreply.github.com>
2025-02-04 15:11:08 +0530

    Delete 30 JAN 2025 directory

--- 30 JAN 2025/CircleDetails.java
+++
@@ -1,8 +0,0 @@
-public class CircleDetails {
-    public static void main(String[] args) {
-        Circle circle = new Circle(11.5);
-        circle.getRadius();
-        circle.area();
-        circle.circumference();
-    }
-}
