441b6abd8aa9e650455f089de2230a9e6467242e
kumarapurv123 <91779441+kumarapurv123@users.noreply.github.com>
2025-02-06 22:09:56 +0530

    deleted file

--- 30 JAN 2025/BookDetails.java
+++
@@ -1,9 +0,0 @@
-public class BookDetails {
-    public static void main(String[] args) {
-        Book book = new Book("C Programming", "Apurv Kumar", 100);
-        book.getAuthorName();
-        book.getTitle();
-        book.getPrice();
-        book.displayDetails();
-    }
-}
--- 30 JAN 2025/EmployeeDetail.java
+++
@@ -1,8 +0,0 @@
-public class EmployeeDetail {
-    public static void main(String[] args) {
-        Employee emp = new Employee("Apurv Kumar", 10, 45000);
-        emp.getName();
-        emp.getID();
-        emp.getSalary();
-    }
-}
